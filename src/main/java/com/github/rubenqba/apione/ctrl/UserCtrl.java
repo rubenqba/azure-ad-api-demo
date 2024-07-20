77d63020f68e347875fac0ce2b13921653dda6b8
Ruben Bresler <111847+rubenqba@users.noreply.github.com>
2024-07-20 11:59:20 -0600

    Creating aspect to handle user access to api resoures

--- src/main/java/com/github/rubenqba/apione/ctrl/UserCtrl.java
+++ src/main/java/com/github/rubenqba/apione/ctrl/UserCtrl.java
@@ -8,8 +8,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
